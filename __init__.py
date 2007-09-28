fee32a687e93adb0966f77af93baf006893b2d25
Florian Schulze <florian.schulze@gmx.net>
2007-09-28 10:43:09 +0000

    Switched to a GS profile for installation. The setup steps will remove obsolete settings from an existing installation.

--- __init__.py
+++ __init__.py
@@ -1,17 +1,1 @@
-"""
-$Id$
-"""
-
-from Products.CMFCore.utils import ToolInit, ContentInit
-
-import RedirectionTool
-
-redirection_tool_globals=globals()
-
-
-def initialize( context ):
-    
-    ToolInit('Redirection Tool', 
-            tools=(RedirectionTool.RedirectionTool, ), 
-            icon='action_icon.gif'
-            ).initialize(context)
+#
---
+++ setuphandlers.py
@@ -0,0 +1,44 @@
+from Products.CMFCore.utils import getToolByName
+
+
+def importVarious(context):
+    """Manual steps not yet covered by GS."""
+    if context.readDataFile('redirectiontool-various.txt') is None:
+        return
+
+    site=context.getSite()
+    logger = context.getLogger("RedirectionTool")
+
+    removeActionProvider(site, logger)
+    removeSkin(site, logger)
+
+
+def removeActionProvider(site, logger):
+    actionTool = getToolByName(site, 'portal_actions', None)
+    if actionTool is not None:
+        actionTool.deleteActionProvider('portal_redirection')
+        logger.info('Removed action provider\n')
+
+
+def removeSkin(site, logger):
+    skinsTool = getToolByName(site, 'portal_skins')
+    # Go through the skin configurations and remove 'redirection_tool'
+    skins = skinsTool.getSkinSelections()
+    for skin in skins:
+        path = skinsTool.getSkinPath(skin)
+        path = [x.strip() for x in path.split(',')]
+        changed = 0
+        new_path = []
+        for layer in path:
+            if layer == 'redirection_tool':
+                changed = 1
+            else:
+                new_path.append(layer)
+
+        if changed:
+            new_path = ', '.join(new_path)
+            # addSkinSelection will replace existing skins as well.
+            skinsTool.addSkinSelection(skin, new_path)
+            logger.info("Removed 'redirection_tool' from %s skin\n" % skin)
+        else:
+            logger.info("Skipping %s skin, 'redirection_tool' was not installed in it\n" % skin)
