6a3fd07a323045dd866c337fa94049f70caa702d
Wouter Vanden Hove <wouter.vanden.hove@gmail.com>
2010-03-29 12:36:22 +0000

    merge i18n-dude-scripts

--- Products/RedirectionTool/compile-po.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-PRODUCTNAME=RedirectionTool
-# Compile po files
-for lang in $(find locales -mindepth 1 -maxdepth 1 -type d); do
-    if test -d $lang/LC_MESSAGES; then
-        msgfmt -o $lang/LC_MESSAGES/${PRODUCTNAME}.mo $lang/LC_MESSAGES/${PRODUCTNAME}.po
-    fi
-done
--- Products/RedirectionTool/rebuild_i18n.sh
+++ Products/RedirectionTool/rebuild_i18n.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 PRODUCTNAME='RedirectionTool'
 I18NDOMAIN=$PRODUCTNAME
 
@@ -8,3 +9,10 @@ i18ndude rebuild-pot --pot locales/${PRODUCTNAME}.pot --merge locales/manual.pot
 # Synchronise the resulting .pot with the .po files
 i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/*/LC_MESSAGES/${PRODUCTNAME}.po
 
+
+# Compile po files
+for lang in $(find locales -mindepth 1 -maxdepth 1 -type d); do
+if test -d $lang/LC_MESSAGES; then
+msgfmt -o $lang/LC_MESSAGES/${PRODUCTNAME}.mo $lang/LC_MESSAGES/${PRODUCTNAME}.po
+fi
+done
